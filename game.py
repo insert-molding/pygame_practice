9c5b4127b3825e5cc46d26756cc71354254d6a1f
mingyukim <coinmagic8085@gmail.com>
2025-05-20 20:08:29 +0900

    feat: 충돌 시 사라지는 아이템 및 이동을 제한하는 장애물 추가

--- game.py
+++ game.py
@@ -22,23 +22,38 @@ red_box = pygame.Rect(red_x, red_y, red_width, red_height)
 # 장애물
 obs_width = obs_height = 50
 obs_x, obs_y = random.randint(0, 800-obs_width), random.randint(0, 600-obs_height)
-obstacle = pygame.Rect(obs_x, obs_y, obs_width, obs_height)
+obstacles = []
+for _ in range(5):
+    obs = pygame.Rect(random.randint(0, 800-obs_width), random.randint(0, 600-50), obs_width, obs_height)
+    obstacles.append(obs)
+    
+        
 
 # 아이템
 item_width = item_height = 25
 item_x, item_y = random.randint(0, 800-item_width), random.randint(0, 600-item_height)
-item = pygame.Rect(item_x, item_y, item_width, item_height)
+items = []
+for _ in range(5):
+    item = pygame.Rect(random.randint(0, 800-item_width), random.randint(0, 600-item_height), item_width, item_height)
+    items.append(item)
+    
+    
+
 
 # fps를 제어할 객체 생성
 clock = pygame.time.Clock()
 fps = 60
-speed = 100
+speed = 300
 
 # 게임 실행 반복문
 running = True
 while running:
     # fps에 따라 속도 조절
     dt_time = speed * (clock.tick(fps) / 1000)
+    
+    # 충돌 시 이동제한
+    previous_position = red_box.copy()
+    # print(previous_position)
     
     # 게임 종료
     for event in pygame.event.get():
@@ -55,12 +70,33 @@ while running:
     if keys[pygame.K_RIGHT]:
         red_box.x += dt_time
     
+    # 충돌 감지
+    for obs in obstacles:
+        if red_box.colliderect(obs):
+            if previous_position.right <= obs.left:
+                # red_box.x = previous_position.x
+                red_box.right = obs.left
+            elif previous_position.left >= obs.right:
+                # red_box.x = previous_position.x
+                red_box.left = obs.right
+            elif previous_position.top >= obs.bottom:
+                # red_box.y = previous_position.y
+                red_box.top = obs.bottom
+            elif previous_position.bottom >= obs.top:
+                # red_box.y = previous_position.y
+                red_box.bottom = obs.top
+    
+    for item in items:
+        if red_box.colliderect(item):
+            items.remove(item)
     # 화면 업데이트
     screen.fill(WHITE)
     
     pygame.draw.rect(screen, RED, red_box)
-    pygame.draw.rect(screen, BLUE, obstacle)
-    pygame.draw.rect(screen, YELLOW, item)
+    for obs in obstacles:
+        pygame.draw.rect(screen, BLUE, obs)
+    for item in items:
+        pygame.draw.rect(screen, YELLOW, item)
     
     pygame.display.flip()
 
